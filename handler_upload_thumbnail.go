d7aed25ad0ca211f1db6b4db268665be5557a4f7
MontillaTomas <montilla.tom1@gmail.com>
2026-03-30 01:28:20 +0000

    Fix thumbnail url

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
 	"os"
 
@@ -46,7 +47,15 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	mediaType := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse media type", err)
+		return
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Unsupported media type", nil)
+		return
+	}
 
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -59,8 +68,8 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	thumbnailURL := cfg.assetsRoot + "/" + videoID.String() + "." + mediaType[len("image/"):]
-	thumbnail, err := os.Create(thumbnailURL)
+	thumbnailURL := "/assets/" + videoID.String() + "." + mediaType[len("image/"):]
+	thumbnail, err := os.Create(cfg.assetsRoot + "/" + videoID.String() + "." + mediaType[len("image/"):])
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create thumbnail file", err)
 		return
